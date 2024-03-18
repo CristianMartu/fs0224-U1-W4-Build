9e77c35a7bb208751a84acda9d9c1f2b3364643f
CristianMartu <martuccicristian18@gmail.com>
2024-03-18 15:19:38 +0100

    welcome-page

--- script/page1-welcome.js
+++ script/page1-welcome.js
@@ -1,1 +1,10 @@
-//const posButton = document.getElementById()
+const posButton = document.getElementById('button-footer')
+const posInput = document.getElementById('check-footer')
+
+posButton.addEventListener('click', (changePage) => {
+  if (posInput.checked) {
+    window.location.href = 'question.html'
+  } else {
+    window.alert('Accettare le condizioni')
+  }
+})
