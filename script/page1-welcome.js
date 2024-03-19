0bcb6120d21c9ea42bb4086a2a68113040cd0f2e
CristianMartu <martuccicristian18@gmail.com>
2024-03-19 09:41:14 +0100

    welcomepage

--- script/page1-welcome.js
+++ script/page1-welcome.js
@@ -1,6 +1,22 @@
 const posButton = document.getElementById('button-footer')
 const posInput = document.getElementById('check-footer')
 
+const submit = (e) => {
+  if (posInput.checked) {
+    posButton.classList.add('inputPressed')
+    posButton.addEventListener('mouseover', (changeColor) => {
+      posButton.classList.add('changeColor')
+    })
+    posButton.addEventListener('mouseleave', (changeColor) => {
+      posButton.classList.remove('changeColor')
+    })
+  } else {
+    posButton.classList.remove('inputPressed')
+  }
+}
+
+posInput.addEventListener('click', submit)
+
 posButton.addEventListener('click', (changePage) => {
   if (posInput.checked) {
     window.location.href = 'question.html'
