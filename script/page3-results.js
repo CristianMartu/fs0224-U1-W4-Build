6331bc42e54b5562e412e03791f49482f755f254
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-03-20 11:10:00 +0100

    spostamenti

--- script/page3-results.js
+++ script/page3-results.js
@@ -1,8 +1,7 @@
-const posButton = document.querySelector('button')
-posButton.addEventListener('click', (changePage) => {
-  window.location.href = 'feedback.html'
-})
-// console.log(question)
-// import { elemento1 } from "./page2-question";
-const result = window.myArray2
-console.log(result)
+const posButton = document.querySelector("button");
+posButton.addEventListener("click", (changePage) => {
+  window.location.href = "feedback.html";
+});
+
+const result = window.myArray2;
+console.log(result);
