5d4d146a69323726a1c18216ea5775df0a16205f
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-03-21 11:36:27 +0100

    change css v3

--- script/page3-results.js
+++ script/page3-results.js
@@ -41,4 +41,4 @@ if (percentageCorrect >= 60) {
     "Ci dispiace! <span style='color: red;'>Non hai passato l'esame.</span>";
 }
 const posCircle = document.getElementById("circle");
-posCircle.style.background = `conic-gradient( #ff0000  ${percentageCorrect}%, #00ffff ${resultWrong}%)`;
+posCircle.style.background = `conic-gradient( #00ffff   ${percentageCorrect}%, #c2128d ${resultWrong}%)`;
