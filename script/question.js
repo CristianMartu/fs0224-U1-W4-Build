39843224964fcb5492e15bd7d95e46d4d8846b67
CristianMartu <martuccicristian18@gmail.com>
2024-03-21 10:32:43 +0100

    question
    spostato le domande in un altra pagina di js e cambiato alcuni dettagli in css

---
+++ script/question.js
@@ -0,0 +1,94 @@
+const questions = [
+  {
+    type: 'multiple',
+    difficulty: 'easy',
+    category: 'Science: Computers',
+    question: 'What is the domain name for the country Tuvalu?',
+    correct_answer: '.tv',
+    incorrect_answers: ['.tu', '.tt', '.tl'],
+  },
+  {
+    type: 'multiple',
+    difficulty: 'easy',
+    category: 'Science: Computers',
+    question: 'How long is an IPv6 address?',
+    correct_answer: '128 bits',
+    incorrect_answers: ['32 bits', '64 bits', '128 bytes'],
+  },
+  {
+    type: 'multiple',
+    difficulty: 'easy',
+    category: 'Science: Computers',
+    question: 'In web design, what does CSS stand for?',
+    correct_answer: 'Cascading Style Sheet',
+    incorrect_answers: [
+      'Counter Strike: Source',
+      'Corrective Style Sheet',
+      'Computer Style Sheet',
+    ],
+  },
+  {
+    type: 'multiple',
+    difficulty: 'easy',
+    category: 'Science: Computers',
+    question:
+      'Which computer hardware device provides an interface for all other connected devices to communicate?',
+    correct_answer: 'Motherboard',
+    incorrect_answers: [
+      'Central Processing Unit',
+      'Hard Disk Drive',
+      'Random Access Memory',
+    ],
+  },
+  {
+    type: 'boolean',
+    difficulty: 'easy',
+    category: 'Science: Computers',
+    question: 'The Windows 7 operating system has six main editions.',
+    correct_answer: 'True',
+    incorrect_answers: ['False'],
+  },
+  {
+    type: 'multiple',
+    difficulty: 'easy',
+    category: 'Science: Computers',
+    question:
+      'If you were to code software in this language you&#039;d only be able to type 0&#039;s and 1&#039;s.',
+    correct_answer: 'Binary',
+    incorrect_answers: ['JavaScript', 'C++', 'Python'],
+  },
+  {
+    type: 'boolean',
+    difficulty: 'easy',
+    category: 'Science: Computers',
+    question:
+      'The programming language &quot;Python&quot; is based off a modified version of &quot;JavaScript&quot;.',
+    correct_answer: 'False',
+    incorrect_answers: ['True'],
+  },
+  {
+    type: 'boolean',
+    difficulty: 'easy',
+    category: 'Science: Computers',
+    question: 'RAM stands for Random Access Memory.',
+    correct_answer: 'True',
+    incorrect_answers: ['False'],
+  },
+  {
+    type: 'boolean',
+    difficulty: 'easy',
+    category: 'Science: Computers',
+    question:
+      'Pointers were not used in the original C programming language; they were added later on in C++.',
+    correct_answer: 'False',
+    incorrect_answers: ['True'],
+  },
+  {
+    type: 'multiple',
+    difficulty: 'easy',
+    category: 'Science: Computers',
+    question: 'On Twitter, what was the original character limit for a Tweet?',
+    correct_answer: '140',
+    incorrect_answers: ['120', '160', '100'],
+  },
+]
