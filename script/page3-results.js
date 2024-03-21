b26a0d6f73dd20f9ff852f7f1d8dd1f206734668
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-03-21 09:21:47 +0100

    update css

--- script/page3-results.js
+++ script/page3-results.js
@@ -1,33 +1,33 @@
-const posButton = document.querySelector('button')
-posButton.addEventListener('click', (changePage) => {
-  window.location.href = 'feedback.html'
-})
+const posButton = document.querySelector("button");
+posButton.addEventListener("click", (changePage) => {
+  window.location.href = "feedback.html";
+});
 
-const stringAnswer = sessionStorage.getItem('correctAnswer')
-const stringMyAnswer = sessionStorage.getItem('myAnswer')
+const stringAnswer = sessionStorage.getItem("correctAnswer");
+const stringMyAnswer = sessionStorage.getItem("myAnswer");
 
-const arrayAnswer = stringAnswer.split(',')
-const arrayMyAnswer = stringMyAnswer.split(',')
+const arrayAnswer = stringAnswer.split(",");
+const arrayMyAnswer = stringMyAnswer.split(",");
 
-corAnswer = []
+corAnswer = [];
 for (let i = 0; i < arrayMyAnswer.length; i++) {
   if (arrayAnswer[i] === arrayMyAnswer[i]) {
-    corAnswer.push(arrayAnswer[i])
+    corAnswer.push(arrayAnswer[i]);
   }
 }
 
-const lenghtAnswer = arrayAnswer.length
-const lengthCorrect = corAnswer.length
-const result = (total, answer) => (100 / total) * answer
-const questionResult = (answer, total) => `${answer}/${total}`
+const lenghtAnswer = arrayAnswer.length;
+const lengthCorrect = corAnswer.length;
+const result = (total, answer) => (100 / total) * answer;
+const questionResult = (answer, total) => `${answer}/${total}`;
 
-const correct1 = document.getElementById('p1')
-correct1.innerText = result(lenghtAnswer, corAnswer.length) + '%'
-const correct2 = document.getElementById('p2')
-correct2.innerText = questionResult(lengthCorrect, lenghtAnswer)
+const correct1 = document.getElementById("p1");
+correct1.innerText = result(lenghtAnswer, corAnswer.length) + "%";
+const correct2 = document.getElementById("p2");
+correct2.innerText = questionResult(lengthCorrect, lenghtAnswer);
 
-const resultWrong = arrayAnswer.length - lengthCorrect
-const wrong1 = document.getElementById('p3')
-wrong1.innerText = result(lenghtAnswer, resultWrong) + '%'
-const wrong2 = document.getElementById('p4')
-wrong2.innerText = questionResult(resultWrong, lenghtAnswer)
+const resultWrong = arrayAnswer.length - lengthCorrect;
+const wrong1 = document.getElementById("p3");
+wrong1.innerText = result(lenghtAnswer, resultWrong) + "%";
+const wrong2 = document.getElementById("p4");
+wrong2.innerText = questionResult(resultWrong, lenghtAnswer);
