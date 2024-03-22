b340fc36e509c73bcf5814f1bb1c750ff1de44c5
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-03-22 10:55:47 +0100

    commenti

--- script/page2-question.js
+++ script/page2-question.js
@@ -1,122 +1,122 @@
-const myAnswer = []
+const myAnswer = [];
 function shuffleArray(array) {
-  return array.sort(() => Math.random() - 0.5)
+  return array.sort(() => Math.random() - 0.5);
 }
 
-const shuffledArray = shuffleArray(questions)
+const shuffledArray = shuffleArray(questions);
 
 const createArrayQuestions = (element) => {
-  const array = []
-  array.push(element.correct_answer)
+  const array = [];
+  array.push(element.correct_answer);
   for (let i = 0; i < element.incorrect_answers.length; i++) {
-    array.push(element.incorrect_answers[i])
+    array.push(element.incorrect_answers[i]);
   }
-  return shuffleArray(array)
-}
+  return shuffleArray(array);
+};
 const createRandomArray = (lenght) => {
-  const listNumber = []
-  let i = 0
+  const listNumber = [];
+  let i = 0;
   while (i < lenght) {
-    const random = Math.floor(Math.random() * lenght)
-    const search = listNumber.includes(random)
+    const random = Math.floor(Math.random() * lenght);
+    const search = listNumber.includes(random);
     if (!search) {
-      listNumber.push(random)
-      i++
+      listNumber.push(random);
+      i++;
     }
   }
-  return listNumber
-}
+  return listNumber;
+};
 
 const corAnswer = () => {
-  const array = []
+  const array = [];
   for (let i = 0; i < questions.length; i++) {
-    array.push(questions[i].correct_answer)
+    array.push(questions[i].correct_answer);
   }
-  return array
-}
+  return array;
+};
 
 const createQuestion = (position, questionElement, index) => {
-  const arrayQuestions = createArrayQuestions(questionElement)
+  const arrayQuestions = createArrayQuestions(questionElement);
 
-  const questionText = document.createElement('h1')
-  questionText.innerText = questionElement.question
-  const conteiner = document.createElement('div')
+  const questionText = document.createElement("h1");
+  questionText.innerText = questionElement.question;
+  const conteiner = document.createElement("div");
   for (let i = 0; i < arrayQuestions.length; i++) {
-    const b = document.createElement('button')
-    b.classList.add('buttons')
-    b.innerText = arrayQuestions[i]
-    conteiner.appendChild(b)
+    const b = document.createElement("button");
+    b.classList.add("buttons");
+    b.innerText = arrayQuestions[i];
+    conteiner.appendChild(b);
   }
-  position.appendChild(questionText)
-  position.appendChild(conteiner)
-  const posP = document.querySelector('footer p')
-  posP.innerHTML = `Question ${index + 1} <code><span>/10</span></code>`
-}
+  position.appendChild(questionText);
+  position.appendChild(conteiner);
+  const posP = document.querySelector("footer p");
+  posP.innerHTML = `Question ${index + 1} <code><span>/10</span></code>`;
+};
 
 const deleteQuestion = () => {
-  const posQ = document.querySelector('h1')
-  const posDiv = document.querySelector('main div')
-  posMain.removeChild(posQ)
-  posDiv.remove()
-}
+  const posQ = document.querySelector("h1");
+  const posDiv = document.querySelector("main div");
+  posMain.removeChild(posQ);
+  posDiv.remove();
+};
 
 const timer = (index, time, progress = 0) => {
-  const totalTime = time
+  const totalTime = time;
   const timerInterval = setInterval(() => {
-    posCircle.style.background = `conic-gradient(#00ffff ${progress}%, #9b9898 0%)`
+    posCircle.style.background = `conic-gradient(rgb(155,152,152, 0.5) ${progress}%, #00ffff 0%)`;
     if (time === -1) {
-      myAnswer.push('Domanda non risposta')
-      clearInterval(timerInterval)
+      myAnswer.push("Domanda non risposta");
+      clearInterval(timerInterval);
     } else if (time === 0) {
       if (index === questions.length) {
-        sessionStorage.setItem('myAnswer', myAnswer)
-        window.location.href = 'results.html'
+        sessionStorage.setItem("myAnswer", myAnswer);
+        window.location.href = "results.html";
       }
-      deleteQuestion()
-      createQuestion(posMain, shuffledArray[index], index)
-      index++
-      changeQuestion(totalTime, index)
-      p.innerText = '0'
+      deleteQuestion();
+      createQuestion(posMain, shuffledArray[index], index);
+      index++;
+      changeQuestion(totalTime, index);
+      p.innerText = "0";
     } else {
-      p.innerText = time
+      p.innerText = time;
     }
-    progress += 100 / totalTime
-    time--
-  }, 1000)
-  return timerInterval
-}
+    progress += 100 / totalTime;
+    time--;
+  }, 1000);
+  return timerInterval;
+};
 
 const changeQuestion = (time, index = 1) => {
-  const startTimer = timer(index, time)
-  const posButton = document.querySelectorAll('.buttons')
+  const startTimer = timer(index, time);
+  const posButton = document.querySelectorAll(".buttons");
   posButton.forEach((element) => {
-    element.classList.add('changeColor')
-    element.addEventListener('click', (change) => {
-      myAnswer.push(element.innerHTML)
+    element.classList.add("changeColor");
+    element.addEventListener("click", (change) => {
+      myAnswer.push(element.innerHTML);
       if (index === shuffledArray.length) {
-        sessionStorage.setItem('myAnswer', myAnswer)
-        window.location.href = 'results.html'
+        sessionStorage.setItem("myAnswer", myAnswer);
+        window.location.href = "results.html";
       } else {
-        clearInterval(startTimer)
-        p.innerText = '0'
-        deleteQuestion()
-        createQuestion(posMain, shuffledArray[index], index)
-        index++
-        changeQuestion(time, index)
+        clearInterval(startTimer);
+        p.innerText = "0";
+        deleteQuestion();
+        createQuestion(posMain, shuffledArray[index], index);
+        index++;
+        changeQuestion(time, index);
       }
-    })
-  })
-}
+    });
+  });
+};
 
-const posMain = document.querySelector('main')
-const posCircle = document.getElementById('circle')
-const p = document.getElementById('timer')
-const correctAnswer = corAnswer()
+const posMain = document.querySelector("main");
+const posCircle = document.getElementById("circle");
+const p = document.getElementById("timer");
+const correctAnswer = corAnswer();
 
-let easyTime = 20
-createQuestion(posMain, shuffledArray[0], 0)
-changeQuestion(easyTime)
+let easyTime = 20;
+createQuestion(posMain, shuffledArray[0], 0);
+changeQuestion(easyTime);
 
-sessionStorage.setItem('correctAnswer', correctAnswer)
+sessionStorage.setItem("correctAnswer", correctAnswer);
 
-console.log(correctAnswer)
+console.log(correctAnswer);
--- script/page4-feedback.js
+++ script/page4-feedback.js
@@ -1,51 +1,52 @@
-const starsContainer = document.getElementById('stelle')
-const feedbackTextElement = document.getElementById('feedback-text')
-const stars = []
+const starsContainer = document.getElementById("stelle"); //dichiaro una variabile che prende l'elemento stelle...
+const feedbackTextElement = document.getElementById("feedback-text"); //ne dichiaro una seconda che assume l'elemento feedback-text
+const stars = [];
 
-// Funzione per creare un elemento p con il testo del feedback
+// Funzione per creare un elemento p con il testo del feedback...
 function createFeedbackParagraph(text) {
-  feedbackTextElement.innerHTML = text
+  feedbackTextElement.innerHTML = text;
 }
 
-// Inizializzazione delle stelle
+// Inizializzazione per creare l'immagine delle stelle e la sua quantità
 for (let i = 0; i < 10; i++) {
-  const newSpan = document.createElement('span')
+  const newSpan = document.createElement("span");
   const svg = `
     <svg width="47" height="46" viewBox="0 0 47 46" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path class="star-path" d="M22.2044 1.55551C22.6143 0.569963 24.0104 0.569964 24.4203 1.55552L29.9874 14.9402C30.1602 15.3557 30.5509 15.6396 30.9994 15.6756L45.4494 16.834C46.5134 16.9193 46.9448 18.2471 46.1341 18.9415L35.1248 28.3722C34.7831 28.6649 34.6338 29.1242 34.7382 29.5619L38.1018 43.6626C38.3494 44.7009 37.2199 45.5215 36.309 44.9651L23.9379 37.4089C23.5538 37.1743 23.0709 37.1743 22.6868 37.4089L10.3157 44.9651C9.40478 45.5215 8.27528 44.7009 8.52295 43.6626L11.8865 29.5619C11.9909 29.1242 11.8416 28.6649 11.4999 28.3722L0.490575 18.9415C-0.320069 18.2471 0.111362 16.9193 1.17535 16.834L15.6253 15.6756C16.0738 15.6396 16.4645 15.3557 16.6374 14.9402L22.2044 1.55551Z" fill="rgb(0,0,0)"/>
     </svg>
-  `
-  newSpan.innerHTML = svg
-  stars.push(newSpan)
-  starsContainer.appendChild(newSpan)
+  `;
+  newSpan.innerHTML = svg;
+  stars.push(newSpan);
+  starsContainer.appendChild(newSpan);
 }
 
-const createClass = document.querySelectorAll('.star-path')
+const createClass = document.querySelectorAll(".star-path"); //vado a selezionare tutte le stelle...
 createClass.forEach((star, index) => {
-  star.addEventListener('click', () => {
+  star.addEventListener("click", () => {
+    //in base al click del mouse avrò un comportamento diverso con i fill dei colori...
     createClass.forEach((star, index2) => {
       if (index >= index2) {
-        star.setAttribute('fill', '#00FFFF')
+        star.setAttribute("fill", "#00FFFF");
       } else {
-        star.setAttribute('fill', 'rgb(0,0,0)')
+        star.setAttribute("fill", "rgb(0,0,0)");
       }
-    })
+    });
 
     // Calcola il numero di stelle cliccate
-    const clickedStars = index + 1
+    const clickedStars = index + 1;
 
     // Mostra il messaggio di feedback appropriato
-    let feedbackText = ''
+    let feedbackText = "";
     if (clickedStars >= 0 && clickedStars <= 5) {
       feedbackText =
-        'We are sorry about your experience with us... \uD83D\uDC4E'
+        "We are sorry about your experience with us... \uD83D\uDC4E"; //aggiungo pollice in giù
     } else if (clickedStars >= 6 && clickedStars <= 8) {
-      feedbackText = 'Thanks for your support! &#128077;' // Aggiunta del pollice in su
+      feedbackText = "Thanks for your support! &#128077;"; // Aggiunta del pollice in su
     } else if (clickedStars >= 9 && clickedStars <= 10) {
-      feedbackText = 'We really appreciate your feedback!  \uD83D\uDE0D'
+      feedbackText = "We really appreciate your feedback!  \uD83D\uDE0D"; //aggiungo emoji faccina..
     }
 
     // Aggiorna il contenuto del messaggio di feedback
-    createFeedbackParagraph(feedbackText)
-  })
-})
+    createFeedbackParagraph(feedbackText);
+  });
+});
