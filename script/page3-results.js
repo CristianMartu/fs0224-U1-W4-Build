f0143c40c3a7dbb712e4051a17843b05e62ae171
DavidePrelati <davide.prelati@gmail.com>
2024-03-22 10:44:56 +0100

    add comments

--- script/page3-results.js
+++ script/page3-results.js
@@ -1,3 +1,4 @@
+// funzione che va a richiamare il bottone, se ci clicchi ti porta alla pagina successiva(feedback)
 const posButton = document.querySelector('button')
 posButton.addEventListener('click', (changePage) => {
   window.location.href = 'feedback.html'
@@ -9,6 +10,7 @@ const stringMyAnswer = sessionStorage.getItem('myAnswer')
 const arrayAnswer = stringAnswer.split(',')
 const arrayMyAnswer = stringMyAnswer.split(',')
 
+// pushiamo all'interno di un array le risposte dell'utente
 corAnswer = []
 for (let i = 0; i < arrayMyAnswer.length; i++) {
   if (arrayAnswer[i] === arrayMyAnswer[i]) {
@@ -21,12 +23,14 @@ const lengthCorrect = corAnswer.length
 const resultPercentage = (total, answer) => (100 / total) * answer
 const questionResult = (answer, total) => `${answer}/${total}`
 
+// richiamiamo il paragrafo per indicare la percentuale delle risposte corrette
 const correct1 = document.getElementById('p1')
 const percentageCorrect = resultPercentage(lenghtAnswer, lengthCorrect)
 correct1.innerText = percentageCorrect + '%'
 const correct2 = document.getElementById('p2')
 correct2.innerText = questionResult(lengthCorrect, lenghtAnswer)
 
+// richiamiamo il paragrafo per indicare la percentuale delle risposte sbagliate
 const resultWrong = arrayAnswer.length - lengthCorrect
 const wrong1 = document.getElementById('p3')
 const percentageWrong = 100 - percentageCorrect
@@ -38,6 +42,7 @@ const messageParagraph = document.getElementById('p5')
 const p1Correct = document.getElementById('p1')
 const p3Wrong = document.getElementById('p3')
 
+// funzione per fare un'animazione al grafico, sia per la circonferenza che per le percentuali
 const animatePercentageText = (totalWrong, totalCorrect) => {
   let currentWrong = 0
   let currentCorrect = 0
@@ -61,7 +66,7 @@ const animatePercentageText = (totalWrong, totalCorrect) => {
     }
   }, 20)
 }
-
+// condizione del risultato finale, se hai passato il test entrerà dentro l'if, altrimenti dentro l'else
 if (percentageCorrect >= 60) {
   messageParagraph.innerHTML =
     "Congratulation! <span style='color: #00BFFF;'>You passed the exam.</span>"
@@ -70,6 +75,7 @@ if (percentageCorrect >= 60) {
     "I'm sorry! <span style='color: red;'>You didn't pass the exam.</span>"
 }
 
+// funzione in caso il punteggio delle risposte corrette e sbagliate sia uguale
 const posCircle = document.getElementById('circle')
 if (percentageCorrect !== percentageWrong) {
   animatePercentageText(percentageWrong, percentageCorrect)
